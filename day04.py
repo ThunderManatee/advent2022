dad876a22d8124251b8534a9f6e7f3ca78221c56
Hunter Nemitz <Hunter@Admins-MacBook-Pro-60.local>
2022-12-07 00:41:17 -0500

    day4 complete

--- day04.py
+++ day04.py
@@ -1,11 +1,12 @@
 from aocd import get_data
 data = [x.split(',') for x in get_data(day=4).splitlines()]
-pairs = 0
+a,b = 0,0
 for x, y in data:
   xMin, xMax = [int(i) for i in x.split('-')]
   yMin, yMax = [int(j) for j in y.split('-')]
-  xr = range(xMin, xMax)
-  yr = range(yMin, yMax)
-  if all(e in yr for e in xr) or all(e in xr for e in yr):
-    pairs = pairs + 1
-  print(xr, yr, pairs)
+  if (xMin <= yMin and yMax <= xMax) or (yMin <= xMin and xMax <= yMax):
+    a = a+1
+  if (range(max(xMin, yMin), min(xMax, yMax)+1)):
+    b = b+1
+print(a,b)
+  
